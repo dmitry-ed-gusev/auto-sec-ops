1090cb55e81158be0936c563a51d5f5e7f67715b
Гусев Дмитрий Эдуардович <de.gusev@severstal.com>
2024-10-25 11:19:32 +0300

    WIP. Folder and resources re-organizing.

--- pycjira/pycjira/pycjira.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-from loguru import logger
-
-logger.info('Test in progress...')
--- shell_scripts/(!!!)_runme.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-source ./project-urls.sh $1 $2
-
-source ./clone-build.sh $PROJECT_URLS
