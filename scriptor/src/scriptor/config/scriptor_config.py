9acb61ecca9542c920e489f04b01319f670b6154
Гусев Дмитрий Эдуардович <de.gusev@severstal.com>
2024-11-05 17:48:39 +0300

    WIP. Adding scripts.

---
+++ scriptor/src/scriptor/config/scriptor_config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+"""
+    Scriptor module configuration.
+
+    Created:  Dmitrii Gusev, 05.11.2024
+    Modified:
+"""
+
+from loguru import logger
---
+++ scriptor/src/scriptor/scriptor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding=utf-8 -*-
+
+"""
+    Scriptor Utility main file (entry point).
+
+    Created:  Dmitrii Gusev, 05.11.2024
+    Modified:
+"""
+
+from loguru import logger
